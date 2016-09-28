add4db9cfe66a2b241821310e66b050d8b544db7
Moyses Nev <moyrene84@gmail.com>
2016-09-27 21:53:55 -0700

    Entities Clase Actualizado

--- AppTemplate/App.Entities/Clase.cs
+++ AppTemplate/App.Entities/Clase.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,9 @@ namespace App.Entities
 {
     public class Clase
     {
-        public int Id_Materia { get; set; }
-        public int Id_Docente { get; set; }
+        [Key]
+        public int Id { get; set; }
+        public int IdMateria { get; set; }
+        public int IdDocente { get; set; }
     }
 }
