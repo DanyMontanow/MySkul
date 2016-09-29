506d95318a8191a7f4cee04ac2ac91fd307070a7
WalterWhite50 <moyrene84@gmail.com>
2016-09-28 18:21:17 -0700

    Publicacion y Materia Modificado

--- AppTemplate/App.Entities/Materia.cs
+++ AppTemplate/App.Entities/Materia.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace App.Entities
 {
     public class Materia
     {
+        [Key]
         public int Id { get; set; }
         public string Nombre { get; set; }
     }
--- AppTemplate/App.Entities/Publicacion.cs
+++ AppTemplate/App.Entities/Publicacion.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-<<<<<<< HEAD
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,26 +7,10 @@ using System.ComponentModel.DataAnnotations;
 namespace App.Entities
 {
     public class Publicacion
-
-
     {
         [Key]
-
         public int Id { get; set; }
         public int IdDocente { get; set; }
-=======
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace App.Entities
-{
-    public class Publicacion
-    { 
-        [Key]
-        public int IDDocente { get; set; }
->>>>>>> Publicacion
         public string Titulo { get; set; }
         public string Descripcion { get; set; }
         public DateTime FechaHora { get; set; }
