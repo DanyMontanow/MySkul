7a371617177013bfd6c37c5febc115c4d75db2c3
Moyses Nev <moyrene84@gmail.com>
2016-10-12 20:28:07 -0700

    AlumnosBLL terminado

---
+++ AppTemplate/App.BLL/AlumnosBLL.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using App.DAL;
+using App.Entities;
+namespace App.BLL
+{
+    public class AlumnosBLL
+    {
+        private AlumnosRepo _alumnoRepo;
+
+        public AlumnosBLL()
+        {
+            _alumnoRepo = new AlumnosRepo();
+        }
+        public void Agregar(Alumno alumno)
+        {
+            _alumnoRepo.Agregar(alumno);
+        }
+        public void Borrar(Alumno alumno)
+        {
+            _alumnoRepo.Borrar(alumno);
+        }
+
+        public List<Alumno> TraerTodo()
+        {
+            return _alumnoRepo.TraerTodo();
+        }
+
+        public Alumno TraerId(int id)
+        {
+            return _alumnoRepo.TraerId(id);
+        }
+    }
+}
