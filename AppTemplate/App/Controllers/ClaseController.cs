cbd593b77ace1356c2fab4dedd1498820f223023
Moyses Nev <moyrene84@gmail.com>
2016-10-18 17:48:58 -0700

    controller terminado ft.Gabriel

---
+++ AppTemplate/App/Controllers/ClaseController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using App.BLL;
+using App.Entities;
+
+namespace App.Controllers
+{
+    public class ClaseController : Controller
+    {
+        private ClaseBLL _claseBLL;
+        private Clase _claseEntity;
+        public ClaseController()
+        {
+            _claseEntity = new Clase();
+            _claseBLL = new ClaseBLL();
+        }
+        // GET: Clase
+        public ActionResult Index()
+        {
+            _claseEntity.IdMateria = 0;
+            _claseEntity.Materia = "";
+
+            return View();
+        }
+    }
+}
