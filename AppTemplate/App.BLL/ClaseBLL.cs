d14082c222c2dd90a4682dd6b44b6f5743fac922
Moyses Nev <moyrene84@gmail.com>
2016-10-18 17:49:45 -0700

    business terminado ft.Gabriel

---
+++ AppTemplate/App.BLL/ClaseBLL.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using App.DAL;
+using App.Entities;
+
+namespace App.BLL
+{
+  public  class ClaseBLL
+    {
+        private ClaseRepo _claseRepo;
+
+        public ClaseBLL()
+        {
+            _claseRepo = new ClaseRepo();
+        }
+        public void Agregar(Clase clase)
+        {
+            _claseRepo.Agregar(clase);
+        }
+        public void Borrar(Clase clase)
+        {
+            _claseRepo.Borrar(clase);
+        }
+
+        public List<Clase> TraerTodo()
+        {
+            return _claseRepo.TraerTodo();
+        }
+
+        public Clase TraerId(int id)
+        {
+            return _claseRepo.TraerId(id);
+        }
+    }
+}
--- AppTemplate/App.BLL/EnteradoBLL.cs
+++ AppTemplate/App.BLL/EnteradoBLL.cs
@@ -3,10 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using App.DAL;
+using App.Entities;
 
 namespace App.BLL
 {
-    class EnteradoBLL
+    public class EnteradoBLL
     {
+        private EnteradoRepo _enteradoRepo;
+
+        public EnteradoBLL()
+        {
+            _enteradoRepo = new EnteradoRepo();
+        }
+        public void Agregar(Enterado enterado)
+        {
+            _enteradoRepo.Agregar(enterado);
+        }
+        public void Borrar(Enterado enterado)
+        {
+            _enteradoRepo.Borrar(enterado);
+        }
+
+        public List<Enterado> TraerTodo()
+        {
+            return _enteradoRepo.TraerTodo();
+        }
+
+        public Enterado TraerId(int id)
+        {
+            return _enteradoRepo.TraerId(id);
+        }
+
     }
 }
